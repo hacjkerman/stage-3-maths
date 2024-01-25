188730b5c6917512945e9f5b538c35e78bc3e0e2
Andy <andrewwang134@gmail.com>
2024-01-25 13:32:25 +1100

    added nav links to addition, subtraction, and multiplication and generated all the numbers required and removed the input once the answer is correct

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -7,61 +7,74 @@ import { Toaster, toast } from "react-hot-toast";
 import PageCSS from "./page.module.css";
 import { generateDivQuestion } from "@/math/division";
 import { generateMultQuestion } from "@/math/multiplication";
+import Nav from "@/components/Nav/Nav";
 export default function Page() {
   const [question, setQuestion] = useState();
   const [answer, setAnswer] = useState();
   const [completed, setCompleted] = useState(false);
   const [topic, setTopic] = useState("addition");
+  const [userInput, setUserInput] = useState();
 
   const topics = {
-    addition: generateAddQuestion(),
-    subtraction: generateMinQuestion(),
-    multiplication: generateMultQuestion(),
-    division: generateDivQuestion,
+    addition: { function: generateAddQuestion(), operation: "+" },
+    subtraction: { function: generateMinQuestion(), operation: "-" },
+    multiplication: { function: generateMultQuestion(), operation: "*" },
+    division: { function: generateDivQuestion(), operation: "%" },
   };
   useEffect(() => {
     async function getQA() {
-      const questionArray = await topics[topic];
+      const questionArray = await topics[topic].function;
       if (questionArray !== undefined) {
-        setQuestion([questionArray[0], questionArray[1]]);
-        setAnswer(questionArray[2]);
+        const questionStr =
+          questionArray.firstNum.toString() +
+          " " +
+          topics[topic].operation +
+          " " +
+          questionArray.secondNum.toString();
+        setQuestion(questionStr);
+        setAnswer(questionArray.answer);
         setCompleted(false);
       }
     }
     getQA();
-  }, [completed]);
+  }, [completed, topic]);
 
   async function onSubmit(event) {
     event.preventDefault();
     const num = event.target[0].value;
     // CHECK ANSWER
     const response = await checkAnswer(Number(num), answer);
-    console.log(response);
     if (response) {
       toast.success("Correct Answer!");
       setCompleted(true);
+      setUserInput("");
     } else {
       toast.error("Incorrect Answer :( Give it another go!");
     }
   }
   let render;
   if (question) {
-    render = (
-      <div className={PageCSS.questionBox}>
-        {question[0]} + {question[1]}
-      </div>
-    );
+    render = <div className={PageCSS.questionBox}>{question}</div>;
   }
   return (
-    <div className={PageCSS.main}>
-      <Toaster />
-      <div>{render}</div>
-      <form onSubmit={onSubmit}>
-        <input type="number" name="answer" className={PageCSS.inputBox} />
-        <button type="submit" className={PageCSS.submitButton}>
-          Submit
-        </button>
-      </form>
-    </div>
+    <>
+      <Nav setTopic={setTopic} />
+      <div className={PageCSS.main}>
+        <Toaster />
+        <div>{render}</div>
+        <form onSubmit={onSubmit}>
+          <input
+            type="number"
+            name="answer"
+            placeholder="Enter your answer here"
+            value={userInput}
+            className={PageCSS.inputBox}
+          />
+          <button type="submit" className={PageCSS.submitButton}>
+            Submit
+          </button>
+        </form>
+      </div>
+    </>
   );
 }
--- frontend/src/math/subtraction.js
+++ frontend/src/math/subtraction.js
@@ -2,5 +2,5 @@ export async function generateMinQuestion() {
   const firstNum = Math.floor(Math.random() * 100);
   const secondNum = Math.floor(Math.random() * 100);
   const answer = firstNum - secondNum;
-  return [firstNum, secondNum, answer];
+  return { firstNum, secondNum, answer };
 }
